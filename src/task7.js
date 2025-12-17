96096435358372f277ccd8cbbe074287d642d38b
Gabriel Flores <gabrielonitsuaf@gmail.com>
2025-12-17 16:55:18 -0600

    Completé el laboratorio de arrays

--- src/task7.js
+++ src/task7.js
@@ -6,6 +6,18 @@ const arr = [
   
   // Type your code below this line!
   
+  //Agregamos el número 99 al final de la primera fila
+  arr[0].push(99);
   
+  // Agregamos una fila completa al final de la matriz
+  arr.push([32,33,34,35,36,37,38,39,40,41]);
+
+  //Eliminamos un número específico (11) de una fila determinada (fila 2)
+  arr[1].splice(1, 1); // Elimina el elemento en el índice 1 de la segunda fila
   
+  //Invertimos una fila sin afectar las demás (fila 3)
+  arr[2].reverse();
+ 
   // Type your code above this line!
+
+  console.log(arr);
